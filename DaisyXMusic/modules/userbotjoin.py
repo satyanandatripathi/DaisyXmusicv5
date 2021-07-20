f52bc93c42cf25a4eea442b2f0c5b44aba7b81f1
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-07-20 19:40:36 +0530

    Update userbotjoin.py

--- DaisyXMusic/modules/userbotjoin.py
+++ DaisyXMusic/modules/userbotjoin.py
@@ -22,5 +22,5 @@ import asyncio
 @Client.on_message(filters.command(["userbotjoin"]) & ~filters.private & ~filters.bot)
 @authorized_users_only
 @errors
-def echo(client, message):
+async def echo(client, message):
     await message.reply_text("this is private project of @mrdark_genuine so if you wanna use this bot then dm at @mrstark_genuine or @RikudoVcPlayer else use @emcee_bot ok!")
