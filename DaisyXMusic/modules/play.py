53d9dbf1b959faf89b341455ceef9ae11de86dca
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-07-31 14:43:15 +0530

    Update play.py

--- DaisyXMusic/modules/play.py
+++ DaisyXMusic/modules/play.py
@@ -605,6 +605,8 @@ async def play(_, message: Message):
                         InlineKeyboardButton("1️⃣", callback_data=f'plll 0|{query}|{user_id}'),
                         InlineKeyboardButton("2️⃣", callback_data=f'plll 1|{query}|{user_id}'),
                         InlineKeyboardButton("3️⃣", callback_data=f'plll 2|{query}|{user_id}'),
+                    ],
+                    [
                         InlineKeyboardButton("4️⃣", callback_data=f'plll 3|{query}|{user_id}'),
                         InlineKeyboardButton("5️⃣", callback_data=f'plll 4|{query}|{user_id}'),
                     ],
@@ -612,6 +614,8 @@ async def play(_, message: Message):
                         InlineKeyboardButton("6️⃣", callback_data=f'plll 5|{query}|{user_id}'),
                         InlineKeyboardButton("7️⃣", callback_data=f'plll 6|{query}|{user_id}'),
                         InlineKeyboardButton("8️⃣", callback_data=f'plll 7|{query}|{user_id}'),
+                    ],
+                    [
                         InlineKeyboardButton("9️⃣", callback_data=f'plll 8|{query}|{user_id}'),
                         InlineKeyboardButton("🔟", callback_data=f'plll 9|{query}|{user_id}'),
                     ],
